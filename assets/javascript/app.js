3c7d447efe8a1f3e065e25f292eb19c48aadae2d
geofell <geofell22@gmail.com>
2019-07-20 19:28:29 -0400

    added border hover around porfolio images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,9 @@
 
+ $(".img-link").hover(
+    function() {
+      $(this).css("border", "5px solid #e76744");
+    },
+    function() {
+      $(this).css("border", "1px solid #dee2e6");
+    }
+  );
